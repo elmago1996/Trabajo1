93882d8234028d26a2e70f8b993a6d96d119a0ca
elmago1996 <pdro4_4_6@hotmail.com>
2015-06-23 20:01:03 -0500

    lkxnv
    oisdnf

--- Pruebas/UnitTest/DataUnitTests.cs
+++ Pruebas/UnitTest/DataUnitTests.cs
@@ -5,12 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using BussinesLayer;
+using DataModeler;
 
 namespace UnitTest
 {
+
     [TestFixture]
     class DataUnitTests
     {
+
+        string ok;
+        string username = "elmago1996";
+        string password = "993800340";
+        string producto_ID = "dffd23";
+        string nombre_producto = "dfgdf";
+
+        bool esperado = true;
+        int stock_esperado = 325;
+
         [Test]
 
         public void Exception()  // TEST DE EXCEPCION CON Nunit 
@@ -19,5 +31,15 @@ namespace UnitTest
             Assert.Throws<ExceptionBL>(() => fun.Exception_divide(5, 0));
         }
 
+        [Test]
+
+        public void DamePassword_UsuarioTest_Nunit()
+        {
+            string user_input = username;
+            string esperado = password;
+            UsuarioBL usuario = new UsuarioBL();
+            string actual = usuario.DamePassword_Usuario(user_input);
+            Assert.AreEqual(esperado, actual);
+        }
     }
 }
